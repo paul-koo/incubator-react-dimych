a5ccc6459e914ace88ffaeb58836a1650e70ded0
paul-koo <pavelkoltun999@gmail.com>
2024-08-07 00:11:03 +0300

    feat: modify accordion component

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -1,49 +1,76 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import { action } from "@storybook/addon-actions"; // callback createar
+import type { StoryObj } from '@storybook/react';
+import { action } from '@storybook/addon-actions'; // callback createar
 
-import { Accordion } from "./Accordion";
-import { useState } from "react";
+import { Accordion } from './Accordion';
+import { useState } from 'react';
 
 export default {
-    title: "Accordion", // название как будет оно отоброжаться в самом story book
+    title: 'Accordion', // название как будет оно отоброжаться в самом story book
     component: Accordion, //сама компонента
 };
 type Story = StoryObj<typeof Accordion>; // метод создания controlled компонента
 
 export const FirtStory: Story = {
     args: {
-        titleValue: "Hello",
+        titleValue: 'Hello',
         collapsed: true,
     },
 };
 
-const onChangeCallback = action("Accordion mode change event fired");
+const onChangeCallback = action('Accordion mode change event fired');
 
 export const CollapsedAccordion = () => {
     return (
         <Accordion
             titleValue="Collapsed"
-            onclick={() => {
+            onChange={() => {
                 onChangeCallback();
             }}
             collapsed={true}
+            items={[
+                { title: 'Dim', value: 1 },
+                { title: 'Anrey', value: 2 },
+                { title: 'Valera', value: 3 },
+            ]}
+            onClick={() => {
+                action('some');
+            }}
         />
     );
 };
 
 export const OpenedAccordion = () => {
-    return <Accordion titleValue="Open" onclick={() => {}} collapsed={false} />;
+    return (
+        <Accordion
+            titleValue="Open"
+            onChange={() => {}}
+            collapsed={false}
+            items={[
+                { title: 'Dim', value: 1 },
+                { title: 'Anrey', value: 2 },
+                { title: 'Valera', value: 3 },
+            ]}
+            onClick={() => action('ss')}
+        />
+    );
 };
 
 export const FullAccordion = () => {
     const [collapsed, setCollapsed] = useState(false);
+    const act = action('some');
     return (
         <Accordion
             titleValue="Rating"
-            onclick={() => {
+            onChange={() => {
                 setCollapsed(!collapsed);
             }}
             collapsed={collapsed}
+            items={[
+                { title: 'Dim', value: 1 },
+                { title: 'Anrey', value: 15 },
+                { title: 'Valera', value: 3 },
+            ]}
+            onClick={act}
         />
     );
 };
--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,14 +1,22 @@
 type AccordionPropsType = {
     titleValue: string;
     collapsed: boolean;
-    onclick: () => void;
+    onChange: () => void;
+    items: Array<ItemType>; // string[]
+    onClick: (value: string) => void;
 };
 
+type ItemType = {
+    title: string;
+    value: any;
+};
 export function Accordion(props: AccordionPropsType) {
     return (
         <>
-            <AccordionTitle title={props.titleValue} onclick={props.onclick} />
-            {!props.collapsed && <AccordionBody />}
+            <AccordionTitle title={props.titleValue} onclick={props.onChange} />
+            {!props.collapsed && (
+                <AccordionBody items={props.items} onClick={props.onClick} />
+            )}
         </>
     );
 }
@@ -22,14 +30,26 @@ function AccordionTitle(props: AccordionTitlePropsType) {
     return <h1 onClick={props.onclick}>{props.title}</h1>;
 }
 
-function AccordionBody() {
+type AccordionBodyPropsType = {
+    items: Array<ItemType>; // string[]
+    onClick: (value: string) => void;
+};
+
+function AccordionBody(props: AccordionBodyPropsType) {
     return (
         <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
+            {props.items.map((el, index) => {
+                return (
+                    <li
+                        key={index}
+                        onClick={() => {
+                            props.onClick(el.value);
+                        }}
+                    >
+                        {el.title}
+                    </li>
+                );
+            })}
         </ul>
     );
 }
